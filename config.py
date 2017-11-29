d7902ea6b21c2852720007e0fca2186a85e4307e
nkalambur <kalamburn@gmail.com>
2017-11-29 17:06:31 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-db_name = "kalambur_507project6" #Enter your db_name here
+db_name = "" #Enter your db_name here
 db_password = "" # Leave this as empty string ""
-db_user = "nikhilkalambur" # Enter your db_user name here
+db_user = "" # Enter your db_user name here
 # change this in github provide notes
